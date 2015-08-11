b60a6633ba049a4064663c61c00a610f73e410a7
Tincho <martin@gta.ufrj.br>
2015-08-11 15:51:23 -0300

    Update Setup-Hbase.sh to set PATH ENV

--- scripts/setup-hbase.sh
+++ scripts/setup-hbase.sh
@@ -2,6 +2,8 @@
 
 source "/vagrant/scripts/common.sh"
 
+HBASE_PATH=/opt/hbase
+
 while getopts t:r: option; do
     case $option in
         t) TOTAL_NODES=$OPTARG;;
@@ -13,7 +15,7 @@ function installHbase {
     downloadApacheFile hbase $HBASE_VERSION_NUM "${HBASE_VERSION}-bin.tar.gz"
 
     tar -oxzf $TARBALL -C /opt
-    safeSymLink "/opt/${HBASE_VERSION}" /opt/hbase
+    safeSymLink "/opt/${HBASE_VERSION}" $HBASE_PATH
 
     mkdir -p /var/log/hbase
 }
@@ -21,8 +23,10 @@ function installHbase {
 function configureHbase {
 
     generateZkStringNoPorts $TOTAL_NODES
-    sed "s/__ZK_QUORUM__/${ZK_STRING_NOPORTS}/" /vagrant/resources/hbase/hbase-site.xml > /opt/hbase/conf/hbase-site.xml
+    sed "s/__ZK_QUORUM__/${ZK_STRING_NOPORTS}/" /vagrant/resources/hbase/hbase-site.xml > $HBASE_PATH/conf/hbase-site.xml
     cp "/vagrant/resources/hbase/supervisor-${HBASE_ROLE}.conf" /etc/supervisor.d/hbase.conf
+    echo "export PATH=$HBASE_PATH/bin:$PATH">>/home/vagrant/.bashrc
+
 }
 
 echo "Setting up HBase"
